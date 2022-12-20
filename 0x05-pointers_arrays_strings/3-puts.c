90628416b613aff7ecf4f60d20553a0266aabc2f
saramal25 <sarahmalainine@gmail.com>
2022-12-19 16:06:18 -0800

    task3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,6 +2,7 @@
 /**
 *_puts - Write a fuction that prints a string followed by a new line to stdouts
 *@str: this is my input string
+*Return: On success 1
 */
 void _puts(char *str)
 {
@@ -10,7 +11,6 @@ while (*str != '\0')
 {
 string++;
 str++;
-_putchar(str[string]);
 }
 _putchar('\n');
 }
